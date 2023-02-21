a507ff73100a5f677a2a18f89a7ac45067b87c16
Aarush1024 <87341588+Aarush1024@users.noreply.github.com>
2023-02-20 18:20:45 -0600

    Add files via upload

---
+++ Pro_137_BoilerPlateCode-main/Pro_137_BoilerPlateCode-main/train_bot.py
@@ -0,0 +1,12 @@
+import nltk
+from nltk.stem import PorterStemmer
+stemmer=PorterStemmer()
+import json
+import pickle
+import numpy as np
+words = []
+classes = []
+word_tags_list = []
+ignore_words = ["?","!",",",".","'s","'m"]
+train_data_file = open("intents.json").read()
+intents = json.loads(train_data_file)
